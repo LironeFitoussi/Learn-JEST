9a88b59daab2e20c1566988562a1e19c66040144
LironeFitoussi <lironefit@gmail.com>
2024-08-14 02:02:46 +0300

    Intro Sections Done

---
+++ JavaScript/Section 2/13/commands.bash
@@ -0,0 +1,8 @@
+# Install vitest as a development dependency
+npm install --save-dev vitest
+
+# set script to run vitest in package.json
+# "scripts": {
+#     "test": "vitest --globals"
+# }
+# --globals flag is used to make sure that the global variables test environment such as "it", "describe", "expect" are available in the test files.
