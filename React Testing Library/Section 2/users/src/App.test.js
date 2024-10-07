09b5ce4e8595bf5d7678514bf675dead3efcfd71
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-07 12:01:48 +0300

    27

--- React Testing Library/Section 2/users/src/App.test.js
+++ React Testing Library/Section 2/users/src/App.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+it('can receive a new user and show it in the list', async () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  await user.click(nameInput);
+  await user.keyboard('Jane Doe');
+
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
+
+  await user.click(button);
+
+  // screen.debug();
+  const name = screen.getByRole('cell', { name: 'Jane Doe' });
+  const email = screen.getByRole('cell', { name: 'jane@jane.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
+  
