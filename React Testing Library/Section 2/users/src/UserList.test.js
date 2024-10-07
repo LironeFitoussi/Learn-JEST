555373718fdea1ce64acd9023d6b66a5df474ab2
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-10-07 11:07:47 +0300

    20

---
+++ React Testing Library/Section 2/users/src/UserList.test.js
@@ -0,0 +1,31 @@
+import { screen, render } from "@testing-library/react";
+import user from "@testing-library/user-event";
+
+import UserList from "./UserList";
+
+describe("UserList Component", () => {
+    const users = [
+        { name: "Jane", email: "jane@dummy.com" },
+        { name: "John", email: "john@dummy.com" },
+    ];
+
+    it("shows a table with the correct headers", () => {
+        // Act
+        render(<UserList users={users} />);
+        const nameHeader = screen.getByText("Name");
+        const emailHeader = screen.getByText("Email");
+
+        // Assert
+        expect(nameHeader).toBeInTheDocument();
+        expect(emailHeader).toBeInTheDocument();
+    });
+
+    it("shows one row for each user", () => {
+        // Act
+        render(<UserList users={users} />);
+        const rows = screen.getAllByRole("row");
+
+        // Assert
+        expect(rows).toHaveLength(users.length + 1);
+    });
+});
